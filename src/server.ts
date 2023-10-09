fc3c109e6c4004746f75c6ee5ed03389c1bdffc4
Sunil Pai <threepointone@gmail.com>
2023-10-09 22:50:38 +0100

    use --define

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,8 @@
 import type * as Party from "partykit/server";
 
 export default class Server implements Party.Server {
-  constructor(readonly party: Party.Party) {}
-
-  onConnect(conn: Party.Connection, ctx: Party.ConnectionContext) {
-    // A websocket just connected!
-    console.log(
-      `Connected:
-  id: ${conn.id}
-  room: ${this.party.id}
-  url: ${new URL(ctx.request.url).pathname}`
-    );
-
-    // let's send a message to the connection
-    conn.send("hello from server");
-  }
-
-  onMessage(message: string, sender: Party.Connection) {
-    // let's log the message
-    console.log(`connection ${sender.id} sent message: ${message}`);
-    // as well as broadcast it to all the other connections in the room...
-    this.party.broadcast(
-      `${sender.id}: ${message}`,
-      // ...except for the connection it came from
-      [sender.id]
-    );
+  static onFetch(request: Party.Request) {
+    return new Response(process.env.MY_VAR + " " + process.env.MY_STR);
   }
 }
 
